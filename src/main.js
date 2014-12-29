18966483e0027017b01398518faf43356b9a94b6
mkyang <303055758@qq.com>
2014-12-29 18:50:36 +0800

    fix bug print error

--- src/main.js
+++ src/main.js
@@ -9,8 +9,7 @@ function main() {
   var zhang = new Person('普通人', '张三', 1000, 200);
   var lisi = new Person('小学生', '李四', 1000, 199, weapon.name, weapon.property, armor.name, armor.property);
   console.log('战斗开始！！！');
-  result = zhang.fight(lisi);
-  console.log(result);
+  zhang.fight(lisi);
 }
 
 main();
--- src/model/person.js
+++ src/model/person.js
@@ -13,33 +13,40 @@ Person.prototype.fight = function (player) {
   var result = '';
   while (1) {
     var proAttack1 = player.attack + player.weaponProperty;
+    var proAttack2 = this.attack - player.armorProperty;
+
     this.hp -= proAttack1;
+    player.hp -= proAttack2;
+
     if (this.hp <= 0) {
       result += player.profession + player.name + '使用最后一击， 把' + this.name + '打死了';
+      console.log(player.profession + player.name + '使用最后一击， 把' + this.name + '打死了');
       break;
     }
+
     result += player.profession + ' ' + player.name + '使用' +
     player.weaponName + '攻击了' + this.profession + ' ' + this.name +
     ',攻击了' + proAttack1 + '点伤害,' + this.name +
     '还剩' + this.hp + '点血';
+
     console.log(player.profession + ' ' + player.name + '使用' +
     player.weaponName + '攻击了' + this.profession + ' ' + this.name +
     ',攻击了' + proAttack1 + '点伤害,' + this.name +
     '还剩' + this.hp + '点血');
 
-    var proAttack2 = this.attack - player.armorProperty;
-    player.hp -= proAttack2;
     if (player.hp <= 0) {
       result += this.profession + this.name + '使用最后一击， 把' + player.name + '死了';
+      console.log(this.profession + this.name + '使用最后一击， 把' + player.name + '死了');
       break;
     }
+
     result += this.profession + ' ' + this.name + '攻击了' + player.profession + player.name +
     ',攻击了' + proAttack2 + '点伤害,' + player.name + '的' + player.armorName + '抵挡了' + player.armorProperty + '点伤害' + player.name +
     '还剩' + player.hp + '点血';
+
     console.log(this.profession + ' ' + this.name + '攻击了' + player.profession + player.name +
     ',攻击了' + proAttack2 + '点伤害,' + player.name + '的' + player.armorName + '抵挡了' + player.armorProperty + '点伤害' + player.name +
     '还剩' + player.hp + '点血');
-
   }
   return result;
 };
